c23ad08f287d47a7085c7d0870b54f06c7fb8d6d
Mariela Atanasova <mariela_2001at@abv.bg>
2018-02-04 13:14:16 +0200

    sortirane

---
+++ bubblesort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 4, 2, 3, 5, 6, 7, 9, 8, 1 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length-1; j++)
+                {
+                    if (arr[j] >arr[j+1])
+                    {
+                        int x = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = x;
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ sort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 4, 2, 3, 5, 6, 7, 9, 8, 1 };
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                int k = 1;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j]) //razmqna
+                    {
+                        k = j;
+                    }
+                }
+                        int x = arr[i];
+                        arr[i] = arr[k];
+                        arr[k] = x;
+            }
+            Console.WriteLine(string.Join("-", arr));
+        }
+    }
+}
